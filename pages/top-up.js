e646accedd965266f5c4c4096ea2b6cce02862d6
1v1ad <1v1ad@proton.me>
2026-01-08 21:47:41 +0300

    Delete pages directory

--- pages/top-up.js
+++
@@ -1,9 +0,0 @@
-
-export default function TopUp() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
-      <h1>Пополнение баланса</h1>
-      <p>Скоро здесь будет форма для пополнения.</p>
-    </div>
-  );
-}
